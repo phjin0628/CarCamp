db3c6bf3a39f0c2c9192a105643f34b55eb29343
qusrmsdud77@naver.com <qusrmsdud77@naver.com>
2021-06-22 14:53:21 +0900

    게시판에 글쓰기 버튼을 달아봐썽

--- app/src/main/java/com/example/myapplication/CommunityActivity.kt
+++ app/src/main/java/com/example/myapplication/CommunityActivity.kt
@@ -1,16 +1,23 @@
 package com.example.myapplication
 
+import android.content.Intent
 import android.media.session.PlaybackState
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.myapplication.databinding.ActivityCommunityBinding
+import com.example.myapplication.databinding.ActivityMainBinding
 import kotlinx.android.synthetic.main.activity_community.*
 
 class CommunityActivity : AppCompatActivity() {
+
+    val binding by lazy { ActivityCommunityBinding.inflate(layoutInflater)}
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_community)
+        setContentView(binding.root)
+        val intent = Intent(this,Community_write::class.java)
+        binding.writeBtn.setOnClickListener{ startActivity(intent) }
 
         val data = loadData()
         val adapter = CustomAdapter()
